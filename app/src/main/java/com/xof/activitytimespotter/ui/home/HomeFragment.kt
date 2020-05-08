8f1b865bfeda80e056f3930d18c171f469e3a85d
Matías Pizarro <matias.pizarro.o96@gmail.com>
2020-05-08 10:59:30 +0200

    trying to connect data from the homeView with the homeFragment and show it in the layout

--- app/src/main/java/com/xof/activitytimespotter/ui/home/HomeFragment.kt
+++ app/src/main/java/com/xof/activitytimespotter/ui/home/HomeFragment.kt
@@ -5,27 +5,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.xof.activitytimespotter.R
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private val homeViewModel: HomeViewModel by activityViewModels()
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.yesterday_date_text)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+
+        val yesterdayDateTextView: TextView = root.findViewById(R.id.yesterday_date_text)
+        homeViewModel.yesterdayDateText.observe(viewLifecycleOwner, Observer {
+            yesterdayDateTextView.text = it
         })
+
+        val todayDateTextView: TextView = root.findViewById(R.id.today_date_text)
+        homeViewModel.todayDateText.observe(viewLifecycleOwner, Observer {
+            todayDateTextView.text = it
+        })
+
+//        val cardView: CardView = CardView()
         return root
     }
 }
--- app/src/main/java/com/xof/activitytimespotter/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/xof/activitytimespotter/ui/home/HomeViewModel.kt
@@ -6,8 +6,25 @@ import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
+    private val _yesterdayDateText = MutableLiveData<String>().apply {
         value = "Ayer, Sábado 2 Mayo 2020"
     }
-    val text: LiveData<String> = _text
+    val yesterdayDateText: LiveData<String> = _yesterdayDateText
+
+    private val _todayDateText = MutableLiveData<String>().apply {
+        value = "Hoy, Domingo 3 Mayo 2020"
+    }
+    val todayDateText: LiveData<String>
+        get() = _todayDateText
+
+    private val _yesterdayTasksNames = MutableLiveData<MutableList<String>>().apply {
+        value = mutableListOf<String>("Dormir", "Jugar COD Warzone")
+    }
+    val yesterdayTasksNames: LiveData<MutableList<String>> = _yesterdayTasksNames
+
+    private val _yesterdayTasksTimes = MutableLiveData<MutableList<String>>().apply {
+        value = mutableListOf<String>("6h34m34s", "1h18m34s")
+    }
+    val yesterdayTasksTimes: LiveData<MutableList<String>>
+        get() = _yesterdayTasksTimes
 }
