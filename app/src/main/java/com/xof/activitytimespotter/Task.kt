083569bc23b2dfc6200a12d8149d6cadcd175628
Matías Pizarro <matias.pizarro.o96@gmail.com>
2020-05-10 19:51:48 +0200

    the class Task was created as template

---
+++ app/src/main/java/com/xof/activitytimespotter/Task.kt
@@ -0,0 +1,54 @@
+package com.xof.activitytimespotter
+
+class Task (var name: String, var time: String)
+
+object Supplier {
+    val yesterday_tasks = listOf<Task>(
+        Task("Dormir", "7h32m12s"),
+        Task("Aprender Kotlin", "2h14m52s"),
+        Task("Ver serie", "1h00m14s"),
+        Task("Jugar Rocket League", "2h32m12s"),
+        Task("Salir a hacer deporte", "1h45m12s"),
+        Task("Ver película", "2h23m12s"),
+        Task("Aprender Kotlin", "2h14m52s"),
+        Task("Ver serie", "1h00m14s"),
+        Task("Jugar Rocket League", "2h32m12s"),
+        Task("Salir a hacer deporte", "1h45m12s"),
+        Task("Ver película", "2h23m12s"),
+        Task("Aprender Kotlin", "2h14m52s"),
+        Task("Ver serie", "1h00m14s"),
+        Task("Jugar Rocket League", "2h32m12s"),
+        Task("Salir a hacer deporte", "1h45m12s"),
+        Task("Ver película", "2h23m12s"),
+        Task("Aprender Kotlin", "2h14m52s"),
+        Task("Ver serie", "1h00m14s"),
+        Task("Jugar Rocket League", "2h32m12s"),
+        Task("Salir a hacer deporte", "1h45m12s"),
+        Task("Ver película", "2h23m12s")
+    )
+
+    val today_tasks = listOf<Task>(
+        Task("213324", "aslkdjfaslk"),
+        Task("54364", "aslkdjfaslk"),
+        Task("213324", "aslkdjfaslk"),
+        Task("2133456543324", "aslkdjfaslk"),
+        Task("5647", "aslkdjfaslk"),
+        Task("213324", "aslkdjfaslk"),
+        Task("354765", "aslkdjfaslk"),
+        Task("213324", "aslkdjfaslk"),
+        Task("35673356", "aslkdjfaslk"),
+        Task("213324", "aslkdjfaslk"),
+        Task("2345322", "aslkdjfaslk"),
+        Task("213324", "aslkdjfaslk"),
+        Task("213324", "aslkdjfaslk"),
+        Task("2133456543324", "aslkdjfaslk"),
+        Task("5647", "aslkdjfaslk"),
+        Task("213324", "aslkdjfaslk"),
+        Task("354765", "aslkdjfaslk"),
+        Task("213324", "aslkdjfaslk"),
+        Task("35673356", "aslkdjfaslk"),
+        Task("213324", "aslkdjfaslk"),
+        Task("2345322", "aslkdjfaslk"),
+        Task("213324", "aslkdjfaslk")
+    )
+}
