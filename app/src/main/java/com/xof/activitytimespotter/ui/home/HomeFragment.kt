61431ab19a8019e8570c95bfd87fc2edffa22eac
Matías Pizarro <matias.pizarro.o96@gmail.com>
2020-05-12 14:02:02 +0200

    recycle implemented inside HomeFragment, now it's possible to display list of tasks

--- app/src/main/java/com/xof/activitytimespotter/ui/home/HomeFragment.kt
+++ app/src/main/java/com/xof/activitytimespotter/ui/home/HomeFragment.kt
@@ -5,15 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.xof.activitytimespotter.R
+import com.xof.activitytimespotter.Supplier
+import com.xof.activitytimespotter.TasksAdapter
 
 class HomeFragment : Fragment() {
 
     private val homeViewModel: HomeViewModel by activityViewModels()
+
+    private lateinit var recyclerViewYesterday: RecyclerView
+    private lateinit var viewAdapterYesterday: RecyclerView.Adapter<*>
+    private lateinit var viewManagerYesterday: RecyclerView.LayoutManager
+    private lateinit var recyclerViewToday: RecyclerView
+    private lateinit var viewAdapterToday: RecyclerView.Adapter<*>
+    private lateinit var viewManagerToday: RecyclerView.LayoutManager
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,12 +38,24 @@ class HomeFragment : Fragment() {
             yesterdayDateTextView.text = it
         })
 
+        viewManagerYesterday = LinearLayoutManager(activity)
+        viewAdapterYesterday = TasksAdapter(Supplier.yesterday_tasks)
+        recyclerViewYesterday = root.findViewById<RecyclerView>(R.id.yesterday_tasks_recycler_view).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+            isFocusable = false
+            // use a linear layout manager
+            layoutManager = viewManagerYesterday
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapterYesterday
+        }
+
         val todayDateTextView: TextView = root.findViewById(R.id.today_date_text)
         homeViewModel.todayDateText.observe(viewLifecycleOwner, Observer {
             todayDateTextView.text = it
         })
 
-//        val cardView: CardView = CardView()
         return root
     }
 }
